dfc4794d6aaf76d1ae0d54253f007f8697550e10
Virginia Dooley <vcdooley@gmail.com>
2018-09-21 09:55:08 +0000

    Done.

--- for.rb
+++ for.rb
@@ -1,7 +1,8 @@
-
 def using_for
 	checklist = 1..10
-	#your code here
+	for item in checklist
+	  puts "Wingardium Leviosa" #=>puts until the phrase 'Wingardium Leviosa' 10 times, once for each item on the checklist"
+	end
 end
 
-
+using_for
